b640398bc98850a8767f741300db79b86bf0ef56
neezecho <neezecho@gmail.com>
2024-12-08 14:32:31 +0200

    no comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div>
-      <h1>Genesis Elearning</h1>
-      <Header /> {/* This is where the Hello component will appear */}
+      <Header />
     </div>
   );
 }
