319311a12d0e3fbdb666e2f881c52729f03f80f0
neezecho <neezecho@gmail.com>
2024-12-27 13:00:25 +0200

    updating changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header className="flex items-center justify-around p-4 bg-gray-100 shadow-md bg-black text-white">
       {/* Logo Section */}
       <span className="flex items-center">
-        <img src="./assets/images/search 1.png" className="h-12"
+        <img src=" " className="h-12"
         />
       </span>
       {/* Search Section */}
@@ -13,7 +13,7 @@ function Header() {
         <input type="text" placeholder="Search..."
         className="px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring focus:ring-blue-200"
         />
-        <img src="../assets/images/search 1.png" alt=''/>
+        <img src=" " alt=''/>
       </span>
       {/* Login Button */}
       <span>
