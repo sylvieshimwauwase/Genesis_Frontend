31396faa78577b9b400ffc4c102e2a28a7ac78bb
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2025-01-13 20:06:57 +0200

    Ignore: Ignored cookies functionalities to make the project work

--- src/constants/httpServices.js
+++ src/constants/httpServices.js
@@ -1,40 +1,47 @@
 import axios from 'axios';
 import apiUrl from './apiUrl';
+// import refreshToken from './refreshToken';
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 10000, // 10 seconds timeout
-  withCredentials: true, // Send cookies with requests
+  // timeout: 10000, // 10 seconds timeout
+  // withCredentials: true, // Send cookies with requests
 });
 
-// Add a request interceptor
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // You can add custom headers here
-    // For example, adding an Authorization token
-    // config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Add a response interceptor
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    if (error.response.status === 401) {
-      // Handle token refresh logic here
-      // Redirect to login if refresh fails
-    }
-    return Promise.reject(error);
-  }
-);
+// // Add a request interceptor
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     // You can add custom headers here
+//     // For example, adding an Authorization token
+//     // config.headers.Authorization = `Bearer ${token}`;
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+//
+// // Add a response interceptor
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     if (error.response.status === 401) {
+//       try {
+//         const newToken = await refreshToken();
+//         error.config.headers['Authorization'] = `Bearer ${newToken}`;
+//         return axiosInstance.request(error.config);
+//       } catch (refreshError) {
+//         console.error('Error in response interceptor:', refreshError);
+//         window.location.href = '/login'; // Redirect to login if refresh fails
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
