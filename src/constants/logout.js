ae85bc123ae6e46c3fc253a16334834edb152782
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2025-01-15 01:49:18 +0200

    Refactor: Improved the logout logic to match the backend

--- src/constants/logout.js
+++ src/constants/logout.js
@@ -1,18 +1,26 @@
 import axios from 'axios';
 import apiUrl from './apiUrl';
 
-const logout = async () => {
+const logout = async (setIsLoading) => {
+    setIsLoading(true);
     try {
-        await axios.post(`${apiUrl}api/logout/`, null, {
+        await axios.post(`${apiUrl}api/logout/`, {
+            refresh: localStorage.getItem('refreshToken'),
+        }, {
             headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`,
             },
         });
     } catch (error) {
         console.error('Error logging out:', error);
+    } finally {
+        localStorage.removeItem("authToken");
+        localStorage.removeItem("refreshToken");
+        localStorage.removeItem("user");
+        document.cookie = 'authToken=; Max-Age=0; path=/;'; // Clear the cookie
+        setIsLoading(false);
+        window.location.href = '/';
     }
-    document.cookie = 'token=; Max-Age=0; path=/;'; // Clear the cookie
-    window.location.href = '/login';
 };
 
 export default logout;
