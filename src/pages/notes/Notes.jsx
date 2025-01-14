b0c00b736360894d1f2daea68251516c78cd0ada
sylvieshimwauwase <slvshimwa@gmail.com>
2025-01-14 14:45:42 +0200

    Feat: Enhance Notes component with dynamic level rendering and loading/error states

--- src/pages/notes/Notes.jsx
+++ src/pages/notes/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import apiService from '../../constants/data.js';
@@ -52,19 +52,21 @@ const Notes = () => {
     setCurrentLevel(level);
   };
 
+  const currentLevelData = levels[currentLevel];
+
   return (
     <div className="flex-grow p-6 bg-gray-100">
       <div className="flex justify-center items-center mt-6">
         <div className="rounded-sm mb-8 max-w-2xl w-full">
           <div className="flex mb-8 max-w-2xl w-full shadow-md">
-            {["P6", "O'Level", "A'Level"].map((level) => (
+            {Object.keys(levels).map((level) => (
               <button
                 key={level}
                 onClick={() => handleLevelChange(level)}
                 className={`flex-1 text-center px-4 py-2 font-semibold ${
                   currentLevel === level
                     ? "bg-[#4175B7] border-[1px] border-[#4175B7] text-white"
-                    : "bg-white border-[1px] border-gray-200 text-gray-700 hover:bg-gray-300'}"
+                    : "bg-white border-[1px] border-gray-200 text-gray-700 hover:bg-gray-300"
                 }`}
                 style={{
                   transition: "background-color 0.3s, color 0.3s",
@@ -98,6 +100,9 @@ const Notes = () => {
           </div>
         </>
       )}
+
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error}</div>}
     </div>
   );
 };
