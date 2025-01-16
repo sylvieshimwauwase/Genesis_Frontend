8a638ec5fe912c326ade43c2f7821c87cdf18b13
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2025-01-16 18:24:59 +0200

    Feat: handling the token refresh on frontend

--- src/constants/httpServices.js
+++ src/constants/httpServices.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 import apiUrl from './apiUrl';
-// import refreshToken from './refreshToken';
 
 const axiosInstance = axios.create({
     baseURL: apiUrl,
     headers: {
         'Content-Type': 'application/json',
     },
-    // timeout: 10000, // 10 seconds timeout
-    // withCredentials: true, // Send cookies with requests
 });
 
 axiosInstance.interceptors.request.use(
@@ -22,37 +19,34 @@ axiosInstance.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
-// // Add a request interceptor
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // You can add custom headers here
-//     // For example, adding an Authorization token
-//     // config.headers.Authorization = `Bearer ${token}`;
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-//
-// // Add a response interceptor
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     if (error.response.status === 401) {
-//       try {
-//         const newToken = await refreshToken();
-//         error.config.headers['Authorization'] = `Bearer ${newToken}`;
-//         return axiosInstance.request(error.config);
-//       } catch (refreshError) {
-//         console.error('Error in response interceptor:', refreshError);
-//         window.location.href = '/login'; // Redirect to login if refresh fails
-//       }
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+const refreshToken = async () => {
+    try {
+        const refreshToken = localStorage.getItem('refreshToken');
+        const response = await axios.post(`${apiUrl}api/token/refresh/`, { refresh: refreshToken });
+        localStorage.setItem('authToken', response.data.access);
+        return response.data.access;
+    } catch (error) {
+        console.error('Failed to refresh token', error);
+        throw error;
+    }
+};
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const newToken = await refreshToken();
+                axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
+                return axiosInstance(originalRequest);
+            } catch (err) {
+                return Promise.reject(err);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
